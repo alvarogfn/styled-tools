db5a13bfc78376b85d6b4690c3af20427289c040
Álvaro <62889807+alvarogfn@users.noreply.github.com>
2024-09-22 13:40:14 -0300

    fix: palette types

--- src/tools/palette/types.ts
+++ src/tools/palette/types.ts
@@ -2,5 +2,9 @@ import type { ComponentProps, ComponentPropsWithTheme } from "@/shared/types.js"
 
 export type ComponentPropsWithPalette<Props extends ComponentProps> = ComponentPropsWithTheme<Props> & {
   palette?: { [key: string]: unknown };
+  theme: {
+    palette?: { [key: string]: unknown };
+    tone?: number;
+  };
   tone?: number;
 };
