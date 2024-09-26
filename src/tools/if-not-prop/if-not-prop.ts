fd1bf43a47ba1812a72284ff0713806dacd4e3be
Álvaro <62889807+alvarogfn@users.noreply.github.com>
2024-09-26 16:18:20 -0300

    feat: improve typescript interfaces

--- src/tools/if-not-prop/if-not-prop.ts
+++ src/tools/if-not-prop/if-not-prop.ts
@@ -1,21 +1,26 @@
-import type { ComponentProps, Interpolation, Needle, StyleFunction } from "@/types/styled-types.js";
+import type { GenericFunction, Needles } from "@/types/styled-types.js";
 
 import { ifProp } from "../if-prop/if-prop.js";
 
 /**
- * Returns `pass` if prop is falsy. Otherwise returns `fail`
+ * Returns `pass` if prop is truthy. Otherwise returns `fail`
+ * @template Props The type of the component props.
+ * @param test the needles that will define whether the test passed or failed
+ * @param pass The value to return or execute if the test is truthy.
+ * @param fail The value to return or execute if the test is falsy.
+ *
  * @example
  * import styled from "styled-components";
- * import { ifNotProp } from "styled-bettertools";
+ * import { ifNotProp } from "styled-bettertools"; // or "styled-bettertools/if-not-prop"
  *
  * const Button = styled.button`
  *   font-size: ${ifNotProp("large", "20px", "30px")};
  * `;
  */
-export function ifNotProp<Props extends ComponentProps>(
-  test: Needle<Props> | Needle<Props>[] | object,
-  pass?: Interpolation<Props>,
-  fail?: Interpolation<Props>,
-): StyleFunction<Props> {
-  return ifProp<Props>(test, fail, pass);
+export function ifNotProp<Props, Pass, Fail>(
+  test: Needles<Props>,
+  pass: Pass | string = "",
+  fail: Fail | string = "",
+): GenericFunction<Props> {
+  return ifProp<Props, Fail, Pass>(test, fail, pass);
 }
